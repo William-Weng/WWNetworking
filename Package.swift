ae2c095a586156dc8af250f720a51d9ccdb4f177
William.Weng <William.Weng@iit.com.hk>
2023-08-22 10:37:12 +0800

    - 將最低版本設定成iOS 14 (符合WWPrint最低標準)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WWNetworking",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v14)
     ],
     products: [
         .library(name: "WWNetworking", targets: ["WWNetworking"]),
